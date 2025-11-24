4d86eac30f8bd003b85334de84651235e3446050
xmavv <xxmatheooo@gmail.com>
2025-11-24 01:27:31 +0100

    feat: computing info from database to display user stats

--- components/statistics/statistics.tsx
+++ components/statistics/statistics.tsx
@@ -1,20 +1,49 @@
 import Statistic from "@/components/statistics/statistic";
+import { History as IHistory } from "@/types/history";
+import { authRequest } from "@/services/auth";
 
-export default function Statistics() {
+export default async function Statistics() {
+  const historyList: IHistory[] = await authRequest("history");
+  const numHistories = historyList.length;
+
+  const matchHistories = historyList.filter(
+    (history) => history.history?.success_percent,
+  );
+  const numMatchHistories = matchHistories.length;
+  const sumMatch = matchHistories.reduce((acc, curr) => {
+    const matchPercent = curr.history.success_percent || 0;
+    return matchPercent + acc;
+  }, 0);
+  const averageMatch =
+    numMatchHistories === 0
+      ? 0
+      : Number((sumMatch / numMatchHistories).toFixed(2)) * 100;
+
+  const methodsUsage: Record<string, number> = {};
+  historyList.forEach((history) => {
+    if (!methodsUsage[history.history.method])
+      methodsUsage[history.history.method] = 0;
+    methodsUsage[history.history.method]++;
+  });
+  const mostUsedMethod =
+    historyList.length < 1
+      ? "NONE"
+      : Object.entries(methodsUsage).sort((a, b) => a[1] - b[1])[0][0];
+
   return (
     <ul>
       <li>
-        <Statistic label="OS" value={"xd"} />
+        <Statistic label="Historical pipelines" value={numHistories} />
       </li>
       <li>
-        <Statistic label="Browser" value={"xd"} />
+        <Statistic label="Average match %" value={`${averageMatch}%`} />
       </li>
       <li>
-        <Statistic label="IP" value={"xd"} />
+        <Statistic label="Most used method" value={mostUsedMethod} />
       </li>
-      <li>
-        <Statistic label="Localization" value={"xd"} />
-      </li>
+      {/*<li>*/}
+      {/*  <Statistic label="Localization" value={"xd"} />*/}
+      {/*</li>*/}
     </ul>
   );
 }
